696127c1299a9d5ed31151ad55a22e48affee777
Josh Streeter <Joshstreeter44@gmail.com>
2022-10-22 21:26:29 -0700

    finished up html and added in dom hooks/variables/starter functions to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,73 +1,85 @@
-/*
- 1. Design UI
-    - Draw a picture
-    - Determine where you will display feedback. 
-    - Determine what is clickable, what will recieve key input, change input
-      timers, scroll events, etc
-    - rough in the elements in HTML, style is less important
+// timer elements
+var timerEl = document.querySelector(".timer");
+var outOfTimeEl = document.querySelector("outOfTime");
+// button elements
+var highScoreButtonEl = document.querySelector(".gameScores");
+var startGameButtonEl = document.querySelector("#startButton");
+var submitScoreButtonEl = document.querySelector("#submitScore");
+var returnButtonEl = document.querySelector("#returnToStart");
+var clearScoresButtonEl = document.querySelector("#clearScores");
+// page elements
+var startingPageEl = document.querySelector(".Start-screen");
+var quizPageEl = document.querySelector(".Quiz");
+var resultsPageEl = document.querySelector(".Results");
+var leaderboardEl = document.querySelector(".Leaderboard");
+// quiz items
+var questionsEl = document.querySelector("#Question");
+var answer1El = document.querySelector("#answer1");
+var answer2El = document.querySelector("#answer2");
+var answer3El = document.querySelector("#answer3");
+var answer4El = document.querySelector("#answer4");
+var correctOrNah = document.querySelector("#Check");
+// other variables
+var correct = 0;
+var questionNumber = 0;
+var finalScore = 0;
+var questionIndex = 0;
+var timer = null;
 
-*/
+// question prompts, choices, and answers
+const questions = [
+  {
+    question: "What is the answer to life, the universe, and everything?",
+    answers: ["1.dumplings", "2.rolling a nat 20", "3.peace of mind", "4.none of the above"],
+    answer: "4.none of the above"
+      
+  },
+  {
+    question: "What is the answer to life, the universe, and everything?",
+    answers: ["1.dumplings", "2.rolling a nat 20", "3.peace of mind", "4.none of the above"],
+    answer: "3.peace of mind"
+      
+  },
+  {
+    question: "What is the answer to life, the universe, and everything?",
+    answers: ["1.dumplings", "2.rolling a nat 20", "3.peace of mind", "4.none of the above"],
+    answer: "2.rolling a nat 20"
+      
+  },
+  {
+    question: "What is the answer to life, the universe, and everything?",
+    answers: ["1.dumplings", "2.rolling a nat 20", "3.peace of mind", "4.none of the above"],
+    answer: "1.dumplings"
+      
+  }
+];
 
-/*
-  2. Declare variables: DOM hooks
-    - In the Javascript, create variables for each of the DOM elements that will display feedback
-    - create variables for the elements that will receive input
-    - set each variable to its DOM element like:
-    
-    var theElement = document.querySelector([CSS Selector for your element]);
- */
-var timerEl = document.querySelector(".gameboard_timer");
-var startGameEl = document.querySelector(".controls-playgame");
-var viewScoresEl = document.querySelector(".controls-highscores");
-var leaderboardEl = document.querySelector(".scoreboard-score");
-var gameboardResultEl = document.querySelector(".gameboard-result");
+// Event page load
+function init(){
+console.log("Game Loading...");
+}
+// event click start 
+function startQuiz(ev){
+console.log("Game started");  
+}
+startGameButtonEl.addEventListener("click", startQuiz);
 
+// event timer tick
+function timerTick(ev){
+  console.log("timer counting!"); 
+}
+// event user answer 
+function userInput() {
+console.log("input answer");
+}
+questionsEl.addEventListener("click", userInput);
 
-/*
- 3. Declare variables: state
-    - What are the datq that need to be kept track of? 
-    - Global state variables sometimes emerge while working on event handlers (i.e., it
-      becomes clearer what needs to be tracked across the application)
-    - state variables:
-      "State describes the status of the entire program or an individual
-       object. It could be text, a number, a boolean, or another data type.
+// event Game ends
+function timeOut(){
 
-       It’s a common tool for coordinating code. For example, once you update state, a bunch of different functions can instantly react to that change."
-       https://www.freecodecamp.org/news/state-in-javascript-explained-by-cooking-a-simple-meal-2baf10a787ee/
-    - Does the state variable need to be global (i.e., used by all the event handlers) or does it only need to be local
-      to the event handler?
-*/
+}
 
-/*
- 4. Declare variables: constants
-    - What are the data the application needs that won't change?
-    - e.g. Math constants, pre-created content (maybe the questions and answers?)
-*/
-
-/* 5. Identify events
-    - Based on the variables created in Step 2, create event handlers
-
-      theElement.addeventListener([EVENT TYPE], function(event){
-        // do stuff here...
-      })
-
-    ...where [EVENT TYPE] is "click" or "change" or "keydown" or whatver
-
-    - Identify the things that should happen in the click handlers
-    - Rememember: there is always a page load event. Usually have a function for anything
-      that needs setting up at the beginning, before people interact with the 
-      page. Start the execution of this setup function at the bottom of page
-*/
-
-// event: page load event
-
-
-/*
- 6. Refactor
-    - identify tasks that can be broken into their own functions, outside the event handlers
-    - Are there tasks that more than one event handler share?
-*/
-
-// start of game
-
+// refactor
+// Start the game 
+init();
 
